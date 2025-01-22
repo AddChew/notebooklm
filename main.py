9d660d1072ed5a40a67de6c7fc7c887015b186b0
AddChew <addison_chew@outlook.com>
2025-01-23 01:30:05 +0800

    interim commit

--- main.py
+++ main.py
@@ -7,45 +7,52 @@ converter = DocumentConverter()
 
 # Widgets
 file_widget = gr.File(
-    file_count = "single", # multiple,
+    file_count = "single",
     file_types = ["pdf"],
     type = "filepath",
     label = "Source"
 )
-submit_widget = gr.Button(
-    value = "Submit",
-    variant = "primary"
-)
 content_widget = gr.Textbox(
-    placeholder = "Parsed document content",
-    label = "Content",
+    label = "Source content",
 )
+script_widget = gr.Textbox(
+    placeholder = "Script",
+    label = "Script",
+)
+audio_widget = gr.Audio()
 
 
 def parse_document(filepath: str):
+    if filepath is None:
+        return None
     result = converter.convert(filepath)
     return result.document.export_to_markdown()
 
 
+def chat(*args, **kwargs):
+    return 'Hello'
+
+
+chat_widget = gr.ChatInterface(chat, examples = ["Summarize the key points in the document"])
+
+
 with gr.Blocks() as demo:
-    with gr.Column():
-        file_widget.render()
-        submit_widget.render()
-        content_widget.render()
-        submit_widget.click(parse_document, inputs = file_widget, outputs = content_widget)
+    gr.Markdown("# NotebookLM")
+    with gr.Row():
+
+        with gr.Column():
+            file_widget.render()
+            content_widget.render()
+            file_widget.change(parse_document, inputs = file_widget, outputs = content_widget)
+
+        with gr.Column():
+            chat_widget.render()
+
+        with gr.Column():
+            gr.Button(value = "Generate podcast")
+            audio_widget.render()
+            script_widget.render()
 
 
 if __name__ == "__main__":
     demo.launch(server_port = 8100)
-
-# test = gr.Interface(
-#     fn = parse_document,
-#     inputs = [file_widget],
-#     outputs = [content_widget]
-# )
-# test2 = gr.Text()
-# demo = gr.TabbedInterface([test, test2], ["Hello World", "Bye World"])
-
-    
-
-#     gr.ChatInterface()
